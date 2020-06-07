f8fe119da255e4bb8e9fad44e3bfe89f655b7e66
liknlie7 <liknlie@gmail.com>
2020-06-07 15:22:09 +0900

    ClearSceneMarge

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,17 +11,33 @@ public class Player : MonoBehaviour
     public float speed = 3.0f;
     // プレイヤーの位置を入れる
     Vector3 playerPos;
-    // Animator
-    Animator anim;
- 
+
+    //================================↓追加分===========================================
+
+    // ゴールしたかどうか
+    bool goalFlag;
+    // オタマジャクシを拾ったかどうか
+    bool tadpoleFlag;
+
+   //=================================↑追加分===========================================
+
     void Start()
     {
         //Rigidbodyを取得
         rb = GetComponent<Rigidbody>();
         //在より少し前の位置を保存
         playerPos = transform.position;
-        // Animator取得
-        anim = GetComponent<Animator>();
+
+        //================================↓追加分===========================================
+
+
+        // オタマジャクシ
+        tadpoleFlag = false;
+        // ゴール
+        goalFlag = false;
+
+        //=================================↑追加分===========================================
+
     }
 
     void Update()
@@ -30,18 +46,17 @@ public class Player : MonoBehaviour
         //A・Dキー、←→キーで横移動
         float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
 
-
         //W・Sキー、↑↓キーで前後移動
         float z = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
 
         //現在の位置＋入力した数値の場所に移動する
         rb.MovePosition(transform.position + new Vector3(x, 0, z));
 
-        //最新の位置から少し前の位置を引いて方向を割り出す
+        //ユニティちゃんの最新の位置から少し前の位置を引いて方向を割り出す
         Vector3 direction = transform.position - playerPos;
 
         //移動距離が少しでもあった場合に方向転換
-        if (direction.magnitude > 0.01f)
+        if (direction.magnitude > 0.05f)
         {
             //directionのX軸とZ軸の方向を向かせる
             transform.rotation = Quaternion.LookRotation(new Vector3
@@ -53,5 +68,43 @@ public class Player : MonoBehaviour
         //位置を更新する
         playerPos = transform.position;
     }
+
+    //=================================↓追加分===========================================
+
+    // ゴールやオタマとの当たり判定
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Goal")
+        {
+            goalFlag = true;
+        }
+
+        if(other.gameObject.tag == "Tadpole")
+        {
+            tadpoleFlag = true;
+           
+        }
+    }
+
+    // ゴールしたフラグ
+    public bool GetGoalFlag()
+    {
+        return goalFlag;
+    }
+
+    // オタマジャクシを拾ったフラグ
+    public bool GetTadpoleFlag()
+    {
+        return tadpoleFlag;
+    }
+
+    // オタマを拾ったフラグの設定
+    public void SetTadpoleFlag(bool flag)
+    {
+        tadpoleFlag = flag;
+    }
+
+    //=================================↑追加分===========================================
+
 }
 
---
+++ Assets/Scripts/Tadpole.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// オタマジャクシ
+public class Tadpole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // プレイヤーと当たったら消す（取得される）
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
