1972a660c40d16764ef974fe74939df0a741cd28
youkiy <youki9083@gmail.com>
2020-06-10 15:17:32 +0900

    CursorStarted

---
+++ Assets/Scripts/WaterSplashCursor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class WaterSplashCursor : MonoBehaviour
+{
+    private GameObject _target;
+
+    private float _minLength;
+    private float _maxLength;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _target = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = Camera.main.WorldToScreenPoint(_target.transform.position);
+    }
+}
