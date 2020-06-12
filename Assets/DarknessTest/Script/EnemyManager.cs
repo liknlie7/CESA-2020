1cc30b1bef57c3d7d42f6ecda535105d15482da0
youkiy <youki9083@gmail.com>
2020-06-12 20:53:25 +0900

    GameOverComplete
    ゲームオーバー時の演出
    プレイヤーアニメーションの微調整
    カーソルのエフェクトを削除(後でプレイヤーの尻尾から出すように変更)
    島Dのやつとコンフリクトする可能性大... : (

---
+++ Assets/DarknessTest/Script/EnemyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    private List<GameObject> _enemyList = new List<GameObject>();
+    
+    // オブジェクト(エネミーの登録)
+    public void RegisterEnemy(GameObject obj)
+    {
+        _enemyList.Add(obj);
+    }
+    
+    // 敵を全て停止させる
+    public void StopAllEnemy()
+    {
+        // 敵を全て停止
+        foreach(GameObject obj in _enemyList)
+        {
+            obj.GetComponent<EnemyController>().StopNavMeshAgent();
+            obj.GetComponent<EnemyController>().enabled = false;
+        }
+    }
+}
