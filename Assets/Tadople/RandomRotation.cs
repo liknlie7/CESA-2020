695fb7675e1a932f03562c7bf9d39c42ae5a06bc
ydeagames <ydeagames@gmail.com>
2020-06-13 20:53:04 +0900

    Todpole Random Rotation

---
+++ Assets/Tadople/RandomRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var euler = transform.localEulerAngles;
+        euler.y = Random.Range(0, 360);
+        transform.localEulerAngles = euler;
+    }
+}
