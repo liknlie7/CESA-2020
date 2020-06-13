2b5b5987c2752a568bf2930f24ebaca4d94972cb
ydeagames <ydeagames@gmail.com>
2020-06-13 22:17:42 +0900

    StopEnemy Rotation

--- Assets/Tadople/RandomRotation.cs
+++ Assets/Tadople/RandomRotation.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class RandomRotation : MonoBehaviour
 {
+    [Range(0, 360)]
+    public float range = 360;
+
     // Start is called before the first frame update
     void Start()
     {
         var euler = transform.localEulerAngles;
-        euler.y = Random.Range(0, 360);
+        euler.y += Random.Range(0, range) - range / 2;
         transform.localEulerAngles = euler;
     }
 }
