22dff78d58ad98e4c92a091d5f60ebc2a4a3b6b5
ydeagames <ydeagames@gmail.com>
2020-06-13 16:27:24 +0900

    Intensity

---
+++ Assets/Scripts/CameraIntensity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraIntensity : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float ambientIntensityPercent;
+    [Range(0, 1)]
+    public float reflectionIntensityPercent;
+
+    public float AmbientIntensity => Mathf.Lerp(ambientIntensityMin, ambientIntensityMax, ambientIntensityPercent);
+    public float ReflectionIntensity => Mathf.Lerp(reflectionIntensityMin, reflectionIntensityMax, reflectionIntensityPercent);
+
+    [Range(0, 8)]
+    public float ambientIntensityMin = 0;
+    [Range(0, 1)]
+    public float reflectionIntensityMin = 0;
+
+    [Range(0, 8)]
+    public float ambientIntensityMax = 1;
+    [Range(0, 1)]
+    public float reflectionIntensityMax = 1;
+
+    void OnPreRender()
+    {
+        RenderSettings.ambientIntensity = AmbientIntensity;
+        RenderSettings.reflectionIntensity = ReflectionIntensity;
+    }
+}
