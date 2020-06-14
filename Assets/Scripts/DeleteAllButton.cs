459dba37d0f3f96962edc6b981b5d56e6c77eedb
yuki4450 <youki9083@gmail.com>
2020-06-14 21:24:16 +0900

    DeleteAllButton

---
+++ Assets/Scripts/DeleteAllButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeleteAllButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+        PlayerPrefs.DeleteAll();
+        this.transform.parent.parent.GetComponent<Animator>().SetTrigger("Refresh");
+    }
+}
